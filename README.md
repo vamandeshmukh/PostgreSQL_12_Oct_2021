96f69fe5f69b43985858748346cc5ddc2563c2fa
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-02 11:23:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PostgreSQL_12_Oct_2021
+ PostgreSQL training for CG trainees 12 Oct 2021 Batch 3 
---
+++ cg12oct.sql
@@ -0,0 +1,118 @@
+-- PostgreSQL training for CG trainees 12 Oct 2021 Batch 3  
+
+-- tutorials -  https://www.postgresqltutorial.com/ 
+
+CREATE TABLE emp (eid INT, ename VARCHAR(10), salary DECIMAL);
+
+SELECT * FROM emp;
+
+SELECT eid, ename, salary FROM emp;
+SELECT ename, salary FROM emp;
+SELECT ename FROM emp;
+
+-- INSERT INTO emp (list of the columns) VALUES (list of the values);
+INSERT INTO emp (eid, ename, salary) VALUES (101, 'Sonu', 45780);
+INSERT INTO emp (eid, ename, salary) VALUES (102, 'Monu', 52140);
+INSERT INTO emp (eid, ename, salary) VALUES (103, 'Tonu', 36560);
+INSERT INTO emp (eid, ename, salary) VALUES (104, 'Ponu', 60500);
+INSERT INTO emp (eid, ename, salary) VALUES (105, 'Gonu', 32000);
+
+COMMIT; 
+
+-- select a specific record - 
+-- What is Sonu's salary? 
+-- clauses 
+SELECT ename, salary FROM emp WHERE ename = 'Sonu';
+SELECT * FROM emp WHERE ename = 'Sonu';
+
+SELECT * FROM emp WHERE salary < 40000;
+
+SELECT * FROM emp WHERE salary > 40000 ORDER BY salary;
+SELECT * FROM emp WHERE salary > 40000 ORDER BY salary DESC;
+
+-- delete records 
+
+SELECT * FROM emp;
+
+DELETE FROM emp WHERE eid = 104;
+
+-- update 
+UPDATE emp SET salary = 55140 WHERE eid = 102;
+
+COMMIT; 
+
+SELECT * FROM emp;
+SELECT MAX(salary) FROM emp; 
+SELECT MIN(salary) FROM emp; 
+SELECT AVG(salary) FROM emp; 
+SELECT MAX(salary), MIN(salary), AVG(salary) FROM emp; 
+
+SELECT ROUND(AVG(salary)) FROM emp; 
+SELECT ROUND(AVG(salary), 2) FROM emp; 
+
+SELECT current_date;
+SELECT current_timestamp;
+SELECT now();
+
+-- 
+
+class CustomerData {
+	int customerId;
+	String customerName;
+}
+
+CREATE TABLE customer_data 
+	 (customer_id INT, 
+	 customer_name VARCHAR(40), 
+	 joining_date DATE, 
+	 address VARCHAR(255));
+
+SELECT * FROM customer_data;
+
+INSERT INTO customer_data 
+	(customer_id, customer_name, joining_date, address) 
+	VALUES (101, 'Sonu', '10-Oct-2020', 'Abids Hyderabad 500001');
+
+INSERT INTO customer_data VALUES 
+	(102, 'Monu', '10-Oct-2020', 'Abids Hyderabad 500001');
+
+INSERT INTO customer_data VALUES 
+	(103, 'Tonu', NULL, 'Abids Hyderabad 500001');
+
+INSERT INTO customer_data VALUES 
+	(104, 'Ponu', '12-31-2019', 'Abids Hyderabad 500001');
+
+INSERT INTO customer_data 
+	(customer_id, customer_name, joining_date) 
+	VALUES (105, 'Sonu', '3-Oct-2020');
+
+-- bad [practice]
+INSERT INTO customer_data 
+	VALUES (105, 'Sonu', '3-Oct-2020');
+
+-- error 
+INSERT INTO customer_data 
+	VALUES (105, 'Sonu', '30-Feb-2020', 'Hyd');
+
+commit;
+
+SELECT * FROM customer_data;
+
+INSERT INTO customer_data 
+	(customer_id, customer_name, joining_date, address) 
+	VALUES (101, 'Sonu', '10-Oct-2020', 'Abids Hyderabad 500001');
+
+INSERT INTO customer_data 
+	(customer_name, joining_date, address) 
+	VALUES ('Monu', '10-Oct-2020', 'Abids Hyderabad 500001');
+
+-- constraints 
+
+CREATE TABLE customer 
+	 (customer_id INT, 
+	 customer_name VARCHAR(40), 
+	 joining_date DATE, 
+	 address VARCHAR(255));
+
+
+
