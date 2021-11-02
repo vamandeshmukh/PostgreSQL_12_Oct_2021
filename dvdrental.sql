c376a14b2cf16a29614e4fbee28098347cc80039
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-02 14:41:46 +0530

    Update dvdrental.sql

--- dvdrental.sql
+++ dvdrental.sql
@@ -1,1 +1,13 @@
 SELECT * FROM customer; 
+SELECT * FROM rental; 
+SELECT * FROM city; 
+
+-- Which customers live in the city 'Haldia'?
+
+SELECT cu.customer_id, cu.first_name, cu.last_Name, ci.city_id, ci.city 
+FROM customer cu 
+JOIN address ad ON cu.address_id = ad.address_id 
+JOIN city ci ON ad.city_id = ci.city_id 
+WHERE ci.city = 'Haldia'; 
+
+
